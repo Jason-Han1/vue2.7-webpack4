0f6d553e169d61ec7c85912c0d40c91f4a4d5bcd
hanlitao <13633260440@163.com>
2023-03-13 18:54:57 +0800

    test

--- client/views/index.vue
+++ client/views/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="wrap" v-waterMarker="{text:'JasonHan版权所有',textColor:'rgba(180, 180, 180, 0.4)'}">
+  <div class="wrap" v-waterMarker="{ text: 'JasonHan版权所有', textColor: 'rgba(180, 180, 180, 0.4)' }">
     <div class="test">
-      <h1>test count: <span class="count">{{count}}</span></h1>
-      <h2>count * 2 = <span class="doubleCount">{{doubleCount}}</span></h2>
+      <h1>test count: <span class="count">{{ count }}</span></h1>
+      <h2>count * 2 = <span class="doubleCount">{{ doubleCount }}</span></h2>
       <!-- <div>state from vuex {{ a }}</div> -->
       <button @click="add">+</button> &nbsp;
       <button @click="cut">-</button>
@@ -27,50 +27,50 @@ import {
 // export default {
 //   name: 'App',
 //   setup(props, { attrs, slots, parent, root, emit, refs }) {
-    const count = ref(0)
-    // const { ctx } = getCurrentInstance()
-    // console.log(ctx.$router.currentRoute.value)
+const count = ref(0)
+// const { ctx } = getCurrentInstance()
+// console.log(ctx.$router.currentRoute.value)
 
-    const add = () => {
-      count.value++
-    }
+const add = () => {
+  count.value++
+}
 
-    const cut = () => {
-      count.value--
-    }
+const cut = () => {
+  count.value--
+}
 
-    // const update = () => {
-    //   ctx.$store.commit('setTestA', count.value)
-    // }
+// const update = () => {
+//   ctx.$store.commit('setTestA', count.value)
+// }
 
-    // const a = computed(() => ctx.$store.state.test.a)
-    const doubleCount = computed(() => count.value * 2)
-    // const token = computed(() => {
-    //   return {...mapState(['token'])}
-    //   })
-    // const token = computed(() => ctx.$store.state.token)
-    watch(() => count.value, val => {
-      console.log('count is change, value is ' + val)
-    })
+// const a = computed(() => ctx.$store.state.test.a)
+const doubleCount = computed(() => count.value * 2)
+// const token = computed(() => {
+//   return {...mapState(['token'])}
+//   })
+// const token = computed(() => ctx.$store.state.token)
+watch(() => count.value, val => {
+  console.log('count is change, value is ' + val)xxx
+})
 
-    onMounted(function() {
-      // console.log(ctx)
-    //   $http({
-    //     url: '/api/auth/login',
-    //     method: 'POST',
-    //     data: {
-    //       account: "123qwe",
-    //       password: "123123"
-    //       }
-    //   }).then(response => {
-    //     console.log(response)
-    //   }, err => {
-    //     console.log(err)
-    //   })
-      // ctx.$store.dispatch('getToken', ()=> {
+onMounted(function () {
+  // console.log(ctx)
+  //   $http({
+  //     url: '/api/auth/login',
+  //     method: 'POST',
+  //     data: {
+  //       account: "123qwe",
+  //       password: "123123"
+  //       }
+  //   }).then(response => {
+  //     console.log(response)
+  //   }, err => {
+  //     console.log(err)
+  //   })
+  // ctx.$store.dispatch('getToken', ()=> {
 
-      // })
-    })
+  // })
+})
 
   //   return {
   //     count,
@@ -92,11 +92,12 @@ import {
   left: 0;
   bottom: 0;
 }
-.count {
-    color: red;
-  }
 
-  .doubleCount {
-    color: greenyellow;
-  }
+.count {
+  color: red;
+}
+
+.doubleCount {
+  color: greenyellow;
+}
 </style>
